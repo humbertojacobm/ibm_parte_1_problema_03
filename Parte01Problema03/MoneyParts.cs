2ed4b85983686b44e373b4bd1d040393667a5bcd
humbertojacobm <humbertojacobm@gmail.com>
2017-11-07 00:39:39 -0500

    f

---
+++ Parte01Problema03/MoneyParts.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte01Problema03
+{
+    public class MoneyParts
+    {
+        
+        public void Built(List<double> coins, List<double> amounts, double highest, double sum, double goal)
+        {
+            // See if we are done.
+            if (sum == goal)
+            {
+                Display(coins, amounts);
+                return;
+            }
+            // See if we have too much.            
+            if (sum > goal)
+            {
+                return;
+            }
+            // Loop through amounts.            
+            foreach (double value in amounts)
+            {
+                // Only add higher or equal amounts.
+                if (value >= highest)
+                {
+                    List<double> copy = new List<double>(coins);
+                    copy.Add(value);
+                    Built(copy, amounts, value, sum + value, goal);
+                }
+            }
+        }
+
+        private void Display(List<double> coins, List<double> amounts)
+        {
+            foreach (double amount in amounts)
+            {
+                int count = coins.Count(value => value == amount);
+                Console.WriteLine("-{0}: {1} vece(s)",
+                    amount,
+                    count);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Parte01Problema03/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte01Problema03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //--------------------------------------
+            //DECLARACION DE VARIABLES
+            //--------------------------------------
+            const string ReadyWord = "LISTO";
+            const string ExitWord = "SALIR";
+            const string MessageOnlyPositiveNumber = "Debe ingresar solamente números positivos";
+            List<double> coins = new List<double>();
+            List<double> amounts = new List<double>() { 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200 };
+
+            MoneyParts _moneyParts = new MoneyParts();
+            double Number = 0;
+
+            //--------------------------------------
+            //INICIO DEL PROGRAMA
+            //--------------------------------------
+            Console.WriteLine("INSTRUCCIONES");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("01. Siempre presione ENTER para avanzar");
+            Console.WriteLine("02. Para salir del programa escriba SALIR (respete las mayúsculas)");
+            Console.WriteLine("03. Ingrese números, presione ENTER para agregar al arreglo");            
+            Console.WriteLine("-------------------------------------");
+
+            string word = Console.ReadLine();            
+
+            while (word != ReadyWord)
+            {
+                if (ExitWord == word)
+                {
+                    break;
+                }                
+                Number = 0;
+
+                bool ResultOfCast = double.TryParse(word, out Number);
+
+                if (ResultOfCast)
+                {
+                    if (Number > 0)
+                    {                        
+                        word = word.Replace('.', ',');
+                        _moneyParts.Built(coins, amounts, 0, 0, Convert.ToDouble(word));
+                    }
+                    else
+                    {
+                        Console.WriteLine(MessageOnlyPositiveNumber);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(MessageOnlyPositiveNumber);
+                }
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Fin de la instrucción");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Ingrese el siguiente número");
+                word = Console.ReadLine();
+            }
+
+            GoOut();
+
+        }        
+
+        static void GoOut()
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("GRACIAS POR UTILIZAR EL PROGRAMA, para salir presione alguna tecla");
+            Console.ReadKey();
+        }
+    }
+}
